464df8ca53c94fd16e6874d7c763db2163e750f6
Ayman Elattar <ayman.320240079@ejust.edu.eg>
2026-07-09 00:07:38 +0300

    Delete ultron directory

--- ultron/hotkey_listener.py
+++
@@ -1,6 +0,0 @@
-import keyboard
-
-def listen(hotkey: str, on_trigger):
-    print(f"Listening for hotkey: {hotkey} (Ctrl+C in this terminal to quit)")
-    keyboard.add_hotkey(hotkey, on_trigger)
-    keyboard.wait()  # blocks forever, listening
